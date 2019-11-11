b08b9d7befc410de5301ee2b2b6311e9f734a69e
Scyrte <32086776+scyrte@users.noreply.github.com>
2019-11-11 22:14:11 +0800

    Update and rename surge zhihu answer.js to zhihu_answer.js

--- QuantumultX/Scripts/zhihu_answer.js
+++ QuantumultX/Scripts/zhihu_answer.js
@@ -5,4 +5,4 @@ body=JSON.stringify(body)
 $done({body})
 
 // 知乎去广告 (By onewayticket255)
-// https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20zhihu%20answer.js
+// https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/zhihu_answer.js
