7cd4356c2e9aa0e2e8b7f5984e190c1ef2b4f160
Scyrte <32086776+scyrte@users.noreply.github.com>
2019-11-11 16:25:41 +0800

    Create surge zhihu answer.js

---
+++ QuantumultX/Scripts/surge zhihu answer.js
@@ -0,0 +1,8 @@
+let body = $response.body
+body=JSON.parse(body)
+delete body['ad_info']
+body=JSON.stringify(body)
+$done({body})
+
+// 知乎去广告 (By onewayticket255)
+// https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20zhihu%20answer.js
