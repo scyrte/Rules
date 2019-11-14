866a3bfc908de43ecba776404d02d1a59a0db0bc
Scyrte <32086776+scyrte@users.noreply.github.com>
2019-11-14 16:04:12 +0800

    Create zhihu-2.js

---
+++ QuantumultX/Scripts/zhihu-2.js
@@ -0,0 +1,17 @@
+let body = $response.body
+
+body=JSON.parse(body)
+
+body['data'].forEach((element, index)=> {
+
+    if(element['type']=='feed_advert'||element['type']=='market_card'){      
+
+       body['data'].splice(index,1)  
+
+    }
+
+})
+
+body=JSON.stringify(body)
+
+$done({body})
