5709abd30c585046ccb624b4273de12ffd234634
Scyrte <32086776+scyrte@users.noreply.github.com>
2019-11-14 14:18:49 +0800

    Update zhihu.js

--- QuantumultX/Scripts/zhihu.js
+++ QuantumultX/Scripts/zhihu.js
@@ -6,21 +6,13 @@
 */
 
 let body = $response.body
-
 body=JSON.parse(body)
-
 body['data'].forEach((element, index)=> {
-
-    if(element['type']=='feed_advert'||element['type']=='market_card'){      
-
-       body['data'].splice(index,1)  
-
+    if(element['type']=='feed_advert'||element['type']=='market_card'){
+       body['data'].splice(index,1)
     }
-
 })
-
 body=JSON.stringify(body)
-
 $done({body})
-
+# 
 # 知乎去广告（By ）
