d73cddc4b023e99a868bb1376ba882c81e239165
Scyrte <32086776+scyrte@users.noreply.github.com>
2019-11-21 23:25:59 +0800

    Update zhihu_answer.js

--- QuantumultX/Scripts/zhihu_answer.js
+++ QuantumultX/Scripts/zhihu_answer.js
@@ -1,8 +1,13 @@
 let body = $response.body
 body=JSON.parse(body)
 delete body['ad_info']
+body['data'].forEach((element, index)=> {
+    if(element['author']['name']=="盐选推荐"){ 
+          body['data'].splice(index,1)  
+     }
+ })
 body=JSON.stringify(body)
 $done({body})
 
 // 知乎去广告 (By onewayticket255)
-// https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/zhihu_answer.js
+// https://github.com/onewayticket255/Surge-Script/blob/master/surge%20zhihu%20answer.js
