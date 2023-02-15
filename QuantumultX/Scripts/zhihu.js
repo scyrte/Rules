cbde0a923c2a62b0a7bb801bad122e40c970cb37
KO <32086776+scyrte@users.noreply.github.com>
2023-02-15 13:59:10 +0800

    Delete QuantumultX/Scripts directory

--- QuantumultX/Scripts/zhihu.js
+++
@@ -1,9 +0,0 @@
-let body = $response.body
-body=JSON.parse(body)
-body['data'].forEach((element, index)=> {
-    if(element['type']=='feed_advert'||element['type']=='market_card'){      
-       body['data'].splice(index,1)  
-    }
-})
-body=JSON.stringify(body)
-$done({body})
